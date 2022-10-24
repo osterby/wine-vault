b86d2283fd3465101c8f735da9ca92bdcd0c6387
Weijing Zhang <weijing.zhang99@gmail.com>
2022-10-24 16:48:08 +1030

    add scaffold-eth folder and update .gitignore

---
+++ scaffold-eth/packages/hardhat/deploy/01_deploy_vault_factory.js
@@ -0,0 +1,31 @@
+// deploy/00_deploy_your_contract.js
+
+const { ethers } = require("hardhat");
+
+const localChainId = "31337";
+
+// const sleep = (ms) =>
+//   new Promise((r) =>
+//     setTimeout(() => {
+//       console.log(`waited for ${(ms / 1000).toFixed(3)} seconds`);
+//       r();
+//     }, ms)
+//   );
+
+module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = await getChainId();
+
+  await deploy("VaultFactory", {
+    // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
+    from: deployer,
+    // args: [ "Hello", ethers.utils.parseEther("1.5") ],
+    log: true,
+    waitConfirmations: 5,
+  });
+
+  // Getting a previously deployed contract
+  const YourContract = await ethers.getContract("VaultFactory", deployer);
+};
+module.exports.tags = ["VaultFactory"];
